436f45b3e9d52f41e4caf6cc8bcd14055fd22580
Alex Prinsen <alexander.f.prinsen@gmail.com>
2024-04-14 11:26:11 -0600

    version update

--- src/App.tsx
+++ src/App.tsx
@@ -310,7 +310,7 @@ export function App() {
         totalTurns={TOTAL_TURNS}
         points={points}
         gameSeed={seed}
-        version="0.3.4"
+        version="0.3.5"
         timerDisplay={displayTime}
         timerWarning={timeRemaining <= 60}
         statusMessage={statusMessage}
